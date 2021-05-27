6631ec00d86dcddbde1475709c9937b1af48b454
Zander Mackie <zmackie@gmail.com>
2021-05-26 21:01:39 -0400

    Something is happening...

--- src/main.rs
+++ src/main.rs
@@ -236,80 +236,80 @@ const CHANNELS: i32 = 2;
 const SAMPLE_RATE: f64 = 44_100.0;
 const FRAMES_PER_BUFFER: u32 = 64;
 
-fn amain() {
-    run().unwrap()
-}
+// fn amain() {
+//     run().unwrap()
+// }
 
-fn arun() -> Result<(), pa::Error> {
+// fn arun() -> Result<(), pa::Error> {
 
-    let pa = pa::PortAudio::new()?;
+//     let pa = pa::PortAudio::new()?;
 
-    // Allocation DSP on the heap
-    let mut dsp = Box::new(mydsp::new());
+//     // Allocation DSP on the heap
+//     let mut dsp = Box::new(mydsp::new());
 
-    println!("Faust Rust code running with Portaudio: sample-rate = {} buffer-size = {}", SAMPLE_RATE, FRAMES_PER_BUFFER);
+//     println!("Faust Rust code running with Portaudio: sample-rate = {} buffer-size = {}", SAMPLE_RATE, FRAMES_PER_BUFFER);
 
-    //Create a input/output stream with the same number of input and output channels
-    const INTERLEAVED: bool = false;// We want NON interleaved streams
-    let input_device = pa.default_input_device()?;
-    let output_device = pa.default_output_device()?;
-    let input_latency = pa.device_info(input_device)?.default_low_input_latency;
-    let output_latency = pa.device_info(output_device)?.default_low_input_latency;
+//     //Create a input/output stream with the same number of input and output channels
+//     const INTERLEAVED: bool = false;// We want NON interleaved streams
+//     let input_device = pa.default_input_device()?;
+//     let output_device = pa.default_output_device()?;
+//     let input_latency = pa.device_info(input_device)?.default_low_input_latency;
+//     let output_latency = pa.device_info(output_device)?.default_low_input_latency;
 
-    let in_params = pa::StreamParameters::new(input_device, CHANNELS, INTERLEAVED, input_latency);
-    let out_params = pa::StreamParameters::new(output_device, CHANNELS, INTERLEAVED, output_latency);
-    let settings = pa::DuplexStreamSettings::new(in_params, out_params, SAMPLE_RATE, FRAMES_PER_BUFFER);
-    //This would have been interleaved:
-    //let mut settings = try!(pa.default_duplex_stream_settings(CHANNELS, CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER));
+//     let in_params = pa::StreamParameters::new(input_device, CHANNELS, INTERLEAVED, input_latency);
+//     let out_params = pa::StreamParameters::new(output_device, CHANNELS, INTERLEAVED, output_latency);
+//     let settings = pa::DuplexStreamSettings::new(in_params, out_params, SAMPLE_RATE, FRAMES_PER_BUFFER);
+//     //This would have been interleaved:
+//     //let mut settings = try!(pa.default_duplex_stream_settings(CHANNELS, CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER));
 
-    println!("get_num_inputs: {}", dsp.get_num_inputs());
-    println!("get_num_outputs: {}", dsp.get_num_outputs());
+//     println!("get_num_inputs: {}", dsp.get_num_inputs());
+//     println!("get_num_outputs: {}", dsp.get_num_outputs());
 
-    // Init DSP with a given SR
-    dsp.init(SAMPLE_RATE as i32);
+//     // Init DSP with a given SR
+//     dsp.init(SAMPLE_RATE as i32);
 
-    //settings.flags = pa::stream_flags::CLIP_OFF;
+//     //settings.flags = pa::stream_flags::CLIP_OFF;
 
-    // This routine will be called by the PortAudio engine when audio is needed. It may called at
-    // interrupt level on some machines so don't do anything that could mess up the system like
-    // dynamic resource allocation or IO.
-    let callback = move |pa::DuplexStreamCallbackArgs { in_buffer, out_buffer, frames, time, .. } : pa::DuplexStreamCallbackArgs<f32, f32>| {
-        let out_buffr: &mut [*mut f32];
-        let in_buffr: & [*const f32];
-        //rust-portaudio does not support non-interleaved audio out of the box (but portaudio does)
-        unsafe {
-            let out_buffer: *mut *mut f32 = ::std::mem::transmute(out_buffer.get_unchecked_mut(0));
-            out_buffr = ::std::slice::from_raw_parts_mut(out_buffer, CHANNELS as usize);
-            let output0 = ::std::slice::from_raw_parts_mut(out_buffr[0], frames);
-            let output1 = ::std::slice::from_raw_parts_mut(out_buffr[1], frames);
+//     // This routine will be called by the PortAudio engine when audio is needed. It may called at
+//     // interrupt level on some machines so don't do anything that could mess up the system like
+//     // dynamic resource allocation or IO.
+//     let callback = move |pa::DuplexStreamCallbackArgs { in_buffer, out_buffer, frames, time, .. } : pa::DuplexStreamCallbackArgs<f32, f32>| {
+//         let out_buffr: &mut [*mut f32];
+//         let in_buffr: & [*const f32];
+//         //rust-portaudio does not support non-interleaved audio out of the box (but portaudio does)
+//         unsafe {
+//             let out_buffer: *mut *mut f32 = ::std::mem::transmute(out_buffer.get_unchecked_mut(0));
+//             out_buffr = ::std::slice::from_raw_parts_mut(out_buffer, CHANNELS as usize);
+//             let output0 = ::std::slice::from_raw_parts_mut(out_buffr[0], frames);
+//             let output1 = ::std::slice::from_raw_parts_mut(out_buffr[1], frames);
 
-            let in_buffer: *const *const f32 = ::std::mem::transmute(in_buffer.get_unchecked(0));
-            in_buffr = ::std::slice::from_raw_parts(in_buffer, CHANNELS as usize);
-            let input0 = ::std::slice::from_raw_parts(in_buffr[0], frames);
-            let input1 = ::std::slice::from_raw_parts(in_buffr[1], frames);
+//             let in_buffer: *const *const f32 = ::std::mem::transmute(in_buffer.get_unchecked(0));
+//             in_buffr = ::std::slice::from_raw_parts(in_buffer, CHANNELS as usize);
+//             let input0 = ::std::slice::from_raw_parts(in_buffr[0], frames);
+//             let input1 = ::std::slice::from_raw_parts(in_buffr[1], frames);
 
-            let inputs = &[input0, input1];
-            let outputs = &mut [output0, output1];
+//             let inputs = &[input0, input1];
+//             let outputs = &mut [output0, output1];
 
-            dsp.compute(frames as i32, inputs, outputs);
-        }
-        pa::Continue
-    };
+//             dsp.compute(frames as i32, inputs, outputs);
+//         }
+//         pa::Continue
+//     };
 
-    let mut stream = pa.open_non_blocking_stream(settings, callback)?;
+//     let mut stream = pa.open_non_blocking_stream(settings, callback)?;
 
-    stream.start()?;
+//     stream.start()?;
 
-    // Wait for user input to quit
-    println!("Press enter/return to quit...");
-    let mut user_input = String::new();
-    io::stdin().read_line(&mut user_input).ok();
+//     // Wait for user input to quit
+//     println!("Press enter/return to quit...");
+//     let mut user_input = String::new();
+//     io::stdin().read_line(&mut user_input).ok();
 
-    stream.stop()?;
-    stream.close()?;
+//     stream.stop()?;
+//     stream.close()?;
 
-    Ok(())
-}
+//     Ok(())
+// }
 
 fn main() {
     nannou::app(model).run();
@@ -356,9 +356,10 @@ fn audio(audio: &mut Audio, buffer: &mut Buffer) {
     // // play_sine(audio, buffer, freq, volume);
     // play_sine(audio, buffer, audio.freq, audio.volume);
     let inputs = &[];
+    let length = 2;
     let mut outputs = buffer.frames_mut().collect::<Vec<&mut[f32]>>();
     
-    audio.dsp.compute(buffer.len_frames() as i32, inputs, &mut outputs)
+    audio.dsp.compute(length, inputs, &mut outputs)
 }
 
 fn play_sine(audio: &mut Audio, buffer: &mut Buffer, freq: f64, volume: f32) {
